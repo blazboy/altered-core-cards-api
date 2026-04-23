6c25610030791ca516cb521a0c8df07ccd260789
John Quairia <john@toxicity.be>
2026-04-23 14:36:54 +0200

    fix collision

---
+++ src/Doctrine/Function/RandomFunction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Doctrine\Function;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\TokenType;
+
+/**
+ * DQL function: RANDOM()
+ * Maps to PostgreSQL: random()
+ */
+class RandomFunction extends FunctionNode
+{
+    public function parse(Parser $parser): void
+    {
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker): string
+    {
+        return 'RANDOM()';
+    }
+}
